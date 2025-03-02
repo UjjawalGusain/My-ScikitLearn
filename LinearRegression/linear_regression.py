f08f3b9bfdbd9eb99a7ebd439c2109c937b3620b
UjjawalGusain <ujjawalgusain31@gmail.com>
2025-03-02 11:44:15 +0530

    Add SGD Regressor

--- LinearRegression/linear_regression.py
+++ LinearRegression/linear_regression.py
@@ -23,3 +23,37 @@ class LinearRegression:
 
         X = np.array(X)
         return np.dot(X, self.coefficients) + self.bias
+
+
+class SGDRegressor:
+    def __init__(self, step=1e-3, epochs=1000):
+        self.bias = None
+        self.coefficients = None
+        self.step = step
+        self.epochs = epochs
+
+    def fit(self, X, y):
+        X = np.array(X)
+        y = np.array(y).ravel()
+        self.bias = 0
+        self.coefficients = np.ones(X.shape[1])
+
+        for epoch in range(self.epochs): 
+
+            indices = np.arange(X.shape[0])  
+            np.random.shuffle(indices)
+            for i in indices:
+                y_hat = np.dot(X[i], self.coefficients) + self.bias
+                slope_bias = y_hat - y[i]
+                self.bias = self.bias - self.step * slope_bias
+
+                slope_coefficients = (y_hat - y[i])* X[i]
+                self.coefficients = self.coefficients - self.step * slope_coefficients
+
+
+    def predict(self, X):
+        assert self.coefficients is not None and self.bias is not None, "Fit the model first"
+
+        X = np.array(X)
+        return np.dot(X, self.coefficients) + self.bias
+
---
+++ LinearRegression/predict_lin_reg.py
@@ -0,0 +1,47 @@
+import pandas as pd
+from sklearn.datasets import load_diabetes
+from sklearn.linear_model import SGDRegressor as SklearnSGDRegressor
+from sklearn.linear_model import LinearRegression as SklearnLinearRegression
+from linear_regression import LinearRegression as MyLinearRegression
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from sklearn.model_selection import train_test_split
+
+data = load_diabetes()
+df = pd.DataFrame(data.data, columns=data.feature_names)
+df['target'] = data.target
+
+X = df.drop(columns=['target'])
+y = df['target']
+df = df.dropna()
+
+
+X = df.drop(columns=['target'])
+y = df['target']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=69, shuffle=True)
+
+print("\n==== LINEAR REGRESSOR COMPARISON ====\n")
+
+myLinear = MyLinearRegression()
+sklearnLinear = SklearnLinearRegression()
+
+myLinear.fit(X_train, y_train)
+sklearnLinear.fit(X_train, y_train)
+
+myLinearPredTrain = myLinear.predict(X_train)
+myLinearPredTest = myLinear.predict(X_test)
+
+sklearnLinearPredTrain = sklearnLinear.predict(X_train)
+sklearnLinearPredTest = sklearnLinear.predict(X_test)
+
+# Evaluate My Linear Regressor
+print(f"My Linear Regression Coefficients: {myLinear.coefficients}, Bias: {myLinear.bias}")
+print(f"My Linear Regression - MSE: {mean_squared_error(y_test, myLinearPredTest)}")
+print(f"My Linear Regression - MAE: {mean_absolute_error(y_test, myLinearPredTest)}")
+print(f"My Linear Regression - R² Score: {r2_score(y_test, myLinearPredTest)}\n")
+
+# Evaluate Sklearn Linear Regressor
+print(f"Sklearn Linear Regression Coefficients: {sklearnLinear.coef_}, Bias: {sklearnLinear.intercept_}")
+print(f"Sklearn Linear Regression - MSE: {mean_squared_error(y_test, sklearnLinearPredTest)}")
+print(f"Sklearn Linear Regression - MAE: {mean_absolute_error(y_test, sklearnLinearPredTest)}")
+print(f"Sklearn Linear Regression - R² Score: {r2_score(y_test, sklearnLinearPredTest)}\n")
